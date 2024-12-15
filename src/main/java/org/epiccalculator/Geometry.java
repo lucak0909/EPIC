737500094e9e0f258bde42de0a51b98acd814d75
lucak0909 <lucak0909@gmail.com>
2024-12-15 16:40:56 +0000

    Finished all functions in shapes for geometry

--- src/main/java/org/epiccalculator/Geometry.java
+++ src/main/java/org/epiccalculator/Geometry.java
@@ -15,8 +15,8 @@ public class Geometry {
     }
 
     public class Rectangle implements Shape2D {
-        private double length;
-        private double width;
+        double length;
+        double width;
 
         public Rectangle(double length, double width) {
             this.length = length;
@@ -41,9 +41,9 @@ public class Geometry {
     }
 
     public class Triangle implements Shape2D {
-        private double a;
-        private double b;
-        private double c;
+        double a;
+        double b;
+        double c;
 
         public Triangle(double a, double b, double c) {
             this.a = a;
@@ -64,7 +64,7 @@ public class Geometry {
     }
 
     public class Circle implements Shape2D {
-        private double radius;
+        double radius;
 
         public Circle(double radius) {
             this.radius = radius;
@@ -80,4 +80,105 @@ public class Geometry {
             return Pi * radius * radius;
         }
     }
+
+    public class Cube implements Shape3D {
+        double side;
+
+        public Cube(double side) {
+            this.side = side;
+        }
+
+        @Override
+        public double calculateVolume() {
+            return Math.pow(side, 3);
+        }
+
+        @Override
+        public double calculateSurfaceArea() {
+            return 6 * side * side;
+        }
+    }
+
+    public class rectangularPrism implements Shape3D {
+        double length;
+        double width;
+        double height;
+
+        public rectangularPrism(double length, double width, double height) {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        @Override
+        public double calculateVolume() {
+            return length * width * height;
+        }
+
+        @Override
+        public double calculateSurfaceArea() {
+            return 2 * ((length * width) + (length * height) + (width * height));
+        }
+    }
+
+    public class Cylinder implements Shape3D {
+        double radius;
+        double height;
+
+        public Cylinder(double radius, double height) {
+            this.radius = radius;
+            this.height = height;
+        }
+
+        @Override
+        public double calculateVolume() {
+            return Pi * Math.pow(radius, 2) * height;
+        }
+
+        @Override
+        public double calculateSurfaceArea() {
+            return 2 * Pi * radius * (radius + height);
+        }
+    }
+
+    public class Sphere implements Shape3D {
+        double radius;
+
+        public Sphere(double radius) {
+            this.radius = radius;
+        }
+
+        @Override
+        public double calculateVolume() {
+            return (4.0 / 3.0) * Pi * Math.pow(radius, 3);
+        }
+
+        @Override
+        public double calculateSurfaceArea() {
+            return 4 * Pi * Math.pow(radius, 2);
+        }
+    }
+
+    public class Cone implements Shape3D {
+        double radius;
+        double height;
+        double baseArea = Pi * radius * radius;
+
+        public Cone(double baseArea, double radius, double height) {
+            this.baseArea = baseArea;
+            this.radius = radius;
+            this.height = height;
+        }
+
+        @Override
+        public double calculateVolume() {
+            return (1.0 / 3.0) * baseArea * height;
+        }
+
+        @Override
+        public double calculateSurfaceArea() {
+            return baseArea + Pi * radius * Math.sqrt(Math.pow(radius, 2) + Math.pow(height, 2));
+        }
+    }
+
 }
