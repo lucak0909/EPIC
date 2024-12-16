df434a099dc7d9779a6d2b882de3a04fb0ceecd6
Oran Dennehy <24418943@studentmail.ul.ie>
2024-12-16 14:33:18 +0000

    main

--- src/main/java/org/epiccalculator/NumberSystems.java
+++ src/main/java/org/epiccalculator/NumberSystems.java
@@ -19,37 +19,37 @@ public class NumberSystems extends Main {
         options();
 
         // Determine Variables to be used
-        byte mode;
-        int number;
-        byte newMode;
+        int mode;
+        double number;
+        int newMode;
         String result = "";
 
         Scanner input = new Scanner(System.in);
-        mode = input.nextByte();
+        mode = input.nextInt();
 
-        // Error handling to make sure user enters byte within range
+        // Error handling to make sure user enters int within range
         while (!Main.Validate.isNumeric(String.valueOf(mode)) || mode > 4 || mode < 1) {
             System.out.println("Invalid -> Try again\n");
             options();
-            mode = input.nextByte();
+            mode = input.nextInt();
         }
 
-        System.out.println("Enter an integer: ");
-        number = input.nextInt();
+        System.out.println("Enter number: ");
+        number = input.nextDouble();
 
         while (!Validate.isNumeric(String.valueOf(number))) {
             System.out.println("Invalid -> Try again\n");
-            System.out.println("Enter your number:");
-            number = input.nextInt();
+            System.out.println("Enter number:");
+            number = input.nextDouble();
         }
 
         options();
-        newMode = input.nextByte();
+        newMode = input.nextInt();
 
         while (!Main.Validate.isNumeric(String.valueOf(newMode)) || newMode > 4 || newMode < 1) {
             System.out.println("Invalid -> Try again\n");
             options();
-            newMode = input.nextByte();
+            newMode = input.nextInt();
         }
 
         NumberSystemConversion conversion = switch (mode) {
@@ -72,18 +72,17 @@ public class NumberSystems extends Main {
 }
 
 interface NumberSystemConversion {
-    String convert(int number, byte newMode);
+    String convert(double number, int newMode);
 }
-
 
 class DecimalConversion implements NumberSystemConversion {
     @Override
-    public String convert(int number, byte newMode) {
+    public String convert(double number, int newMode) {
         return switch (newMode) {
             case 1 -> String.valueOf(number);
-            case 2 -> Integer.toBinaryString(number);
-            case 3 -> Integer.toHexString(number);
-            case 4 -> Integer.toOctalString(number);
+            case 2 -> Integer.toBinaryString((int) number);
+            case 3 -> Integer.toHexString((int) number);
+            case 4 -> Integer.toOctalString((int) number);
             default -> "";
         };
     }
@@ -91,11 +90,11 @@ class DecimalConversion implements NumberSystemConversion {
 
 class BinaryConversion implements NumberSystemConversion {
     @Override
-    public String convert(int number, byte newMode) {
-        int binary = Integer.parseInt(String.valueOf(number), 2);
+    public String convert(double number, int newMode) {
+        int binary = Integer.parseInt(String.valueOf((int) number), 2);
         return switch (newMode) {
             case 1 -> String.valueOf(binary);
-            case 2 -> String.valueOf(number);
+            case 2 -> String.valueOf((int) number);
             case 3 -> Integer.toHexString(binary);
             case 4 -> Integer.toOctalString(binary);
             default -> "";
@@ -105,12 +104,12 @@ class BinaryConversion implements NumberSystemConversion {
 
 class HexadecimalConversion implements NumberSystemConversion {
     @Override
-    public String convert(int number, byte newMode) {
-        int hex = Integer.parseInt(String.valueOf(number), 16);
+    public String convert(double number, int newMode) {
+        int hex = Integer.parseInt(String.valueOf((int) number), 16);
         return switch (newMode) {
             case 1 -> String.valueOf(hex);
             case 2 -> Integer.toBinaryString(hex);
-            case 3 -> String.valueOf(number);
+            case 3 -> String.valueOf((int) number);
             case 4 -> Integer.toOctalString(hex);
             default -> "";
         };
@@ -119,17 +118,14 @@ class HexadecimalConversion implements NumberSystemConversion {
 
 class OctalConversion implements NumberSystemConversion {
     @Override
-    public String convert(int number, byte newMode) {
-        int octal = Integer.parseInt(String.valueOf(number), 8);
+    public String convert(double number, int newMode) {
+        int octal = Integer.parseInt(String.valueOf((int) number), 8);
         return switch (newMode) {
             case 1 -> String.valueOf(octal);
             case 2 -> Integer.toBinaryString(octal);
             case 3 -> Integer.toHexString(octal);
-            case 4 -> String.valueOf(number);
+            case 4 -> String.valueOf((int) number);
             default -> "";
         };
     }
 }
-
-
-
